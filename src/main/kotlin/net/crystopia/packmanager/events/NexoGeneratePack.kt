b4714e3a92055c074bd8431281ee0e191f28d870
Jesper <jesper.l.richert@gmail.com>
2025-04-03 10:26:52 +0200

    Fixed Versioning

--- src/main/kotlin/net/crystopia/packmanager/events/NexoGeneratePack.kt
+++ src/main/kotlin/net/crystopia/packmanager/events/NexoGeneratePack.kt
@@ -4,6 +4,7 @@ import com.nexomc.nexo.api.events.resourcepack.NexoPostPackGenerateEvent
 import net.crystopia.packmanager.config.ConfigManager
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import java.io.File
 import java.util.UUID
 
 object NexoGeneratePack : Listener {
@@ -11,38 +12,57 @@ object NexoGeneratePack : Listener {
     @EventHandler
     fun onGenPackEvent(event: NexoPostPackGenerateEvent) {
 
-        println("Generating pack...")
+        println("[PackManager] Loading Extra Content for Crystopia")
+        println("[PackManager] Loading Versioning...")
 
         val config = ConfigManager.settings.packMode
         val id = UUID.randomUUID().toString().split("-")[0]
 
         if (config == "Development") {
-            println("Development mode generated.")
+            println("[PackManager] Development mode generated.")
             val packMcMeta = """
-            {
-             "pack": {
-             "pack_format": 15,
-             "description": "v.${id}-dev"
-             }
-            }
-            """.trimIndent().toByteArray(Charsets.UTF_8)
+        {
+         "pack": {
+         "pack_format": 15,
+         "description": "v.${id}-dev"
+         }
+        }
+    """.trimIndent().toByteArray(Charsets.UTF_8)
 
-            val newfile = event.addUnknownFile("pack.mcmeta", packMcMeta)
-            println(newfile)
+            val packDir = File("plugins/Nexo/pack")
+            if (!packDir.exists()) {
+                packDir.mkdirs()
+            }
+
+
+            val newFile = File(packDir, "pack.mcmeta")
+            newFile.writeBytes(packMcMeta)
 
         } else if (config == "Production") {
-            val packMcMeta = """
-            {
-             "pack": {
-             "pack_format": 15,
-             "description": "v.${id}"
-             }
-            }
-            """.trimIndent().toByteArray(Charsets.UTF_8)
+            println("[PackManager] Production mode generated.")
 
-            event.addUnknownFile("pack.mcmeta", packMcMeta)
+            val packMcMeta = """
+        {
+         "pack": {
+         "pack_format": 15,
+         "description": "v.${id}"
+         }
+        }
+    """.trimIndent().toByteArray(Charsets.UTF_8)
+
+
+            val packDir = File("plugins/Nexo/pack")
+            if (!packDir.exists()) {
+                packDir.mkdirs()
+            }
+
+            val newFile = File(packDir, "pack.mcmeta")
+            newFile.writeBytes(packMcMeta)
         } else {
             return
         }
+
+
+        println("[PackManager] Generating and patching done.")
     }
 }
